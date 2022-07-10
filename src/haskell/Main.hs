c230b87289b47295bbc60a0b0bb2d623d4392e05
Luiz Antonio <48871407+FallRB@users.noreply.github.com>
2022-07-09 21:55:07 -0300

    Create Main.hs

---
+++ src/haskell/Main.hs
@@ -0,0 +1,29 @@
+import System.Directory
+import TetrisGrafico
+
+main:: IO()
+main = do
+  pontuacao <- getPontuacao
+  pontuacaoNova <- jogarOJogo pontuacao
+  guardarPontuacao pontuacaoNova
+
+getPontuacao:: IO [Int]
+getPontuacao = do
+  temArquivo <- doesFileExist arquivoPontuacao
+  if not temArquivo then return [] else calcularPontuacao <$> readFile arquivoPontuacao
+
+guardarPontuacao:: [Int] -> IO()
+guardarPontuacao = writeFile arquivoPontuacao . show . take pontuacaoMaxima . filter (> 0)
+
+arquivoPontuacao:: FilePath
+arquivoPontuacao = "pontuacao"
+
+pontuacaoMaxima:: Int
+pontuacaoMaxima = 10
+
+calcularPontuacao:: String -> [Int]
+calcularPontuacao pont
+  | null lerPontuacao = []
+  | otherwise = fst . head $ lerPontuacao
+  where
+    lerPontuacao = reads pont
