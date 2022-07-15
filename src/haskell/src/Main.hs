bf0643e623d96e29e14105d4dc7e13411fa266d8
Luiz Antonio <48871407+FallRB@users.noreply.github.com>
2022-07-14 22:31:04 -0300

    Update Main.hs

--- src/haskell/src/Main.hs
+++ src/haskell/src/Main.hs
@@ -9,20 +9,25 @@ main = do
   pontuacaoNova <- iniciarJogo pontuacao
   guardarPontuacao pontuacaoNova
 
+-- pega a pontuação do arquivo onde ela fica
 getPontuacao:: IO [Int]
 getPontuacao = do
   temArquivo <- doesFileExist arquivoPontuacao
   if not temArquivo then return [] else calcularPontuacao <$> readFile arquivoPontuacao
 
+-- escreve a pontuação em um arquivo para guardá-la
 guardarPontuacao:: [Int] -> IO()
 guardarPontuacao = writeFile arquivoPontuacao . show . take pontuacaoMaxima . filter (> 0)
 
+-- filepath do arquivo que guarda a pontuação
 arquivoPontuacao:: FilePath
 arquivoPontuacao = "pontuacao"
 
+-- definição da pontuação máxima
 pontuacaoMaxima:: Int
 pontuacaoMaxima = 10
 
+-- Calcula a pontuação do jogo 
 calcularPontuacao:: String -> [Int]
 calcularPontuacao pont
   | null lerPontuacao = []
